ace19e75253141f5269d1efcff5295b955deaaf1
Iwan Cornelius <iwan@amentum.com.au>
2024-08-22 17:46:06 +1000

    salinity_*.png

--- black_sea/main.py
+++ black_sea/main.py
@@ -1,6 +1,7 @@
 import map_plotter
 import async_api_caller
 import numpy as np 
+import numpy.ma as ma
 import os
 from datetime import datetime
 
@@ -9,7 +10,7 @@ lat_min, lon_min = 41.06967946701225, 27.658566744859286
 lat_max, lon_max = 46.97335743986021, 42.82746724576293
 
 # Figure 1 salinity maps at different depths 
-res = 1 # deg (change to 0.1 deg)
+res = 0.1 # deg (change to 0.1 deg)
 lons_l = np.arange(lon_min, lon_max, res)
 lats_l = np.arange(lat_min, lat_max, res)
 lons_g, lats_g = np.meshgrid(lons_l, lats_l, indexing='ij')
@@ -18,7 +19,9 @@ lons_f = lons_g.flatten()
 lats_f = lats_g.flatten()
 
 url = "https://ocean.amentum.io/nemo/phys"
-headers = {'API-Key': os.getenv("API_KEY")}
+key = os.getenv("API_KEY")
+if key is None: ValueError("set env var API_KEY")
+headers = {'API-Key': key}
 variable = "so"
 dt = datetime(2024, 8, 21)
 
@@ -40,7 +43,19 @@ param_list = [
     for (lat, lon) in zip(lats_f, lons_f)
 ]
 
-# Send it! Bombard API with requests in an asyncronous fashion (order maintained by the package)
+# Send it! Bombard API with requests in an asyncronous fashion 
+# (order maintained by the package, automagically cached)
 responses_json = async_api_caller.run(
     url, headers, param_list
 )
+
+salinities = [r['value'] for r in responses_json]
+
+salinities = np.reshape(np.array(salinities), lats_g.shape).astype(float)
+
+# over land points will be None 
+masked_salinities = ma.masked_invalid(salinities)
+
+map_plotter.plot(lons_g, lats_g, masked_salinities,  
+                 units="PSU", img_name=f"salinity_{depth}.png", 
+                 save=True)
