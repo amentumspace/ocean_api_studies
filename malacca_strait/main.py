34f83e00fd7c9105ac6dd10a644d899c7263167a
Iwan Cornelius <iwan@amentum.com.au>
2025-07-05 10:34:25 +1000

    added streamlit-comparison

--- malacca_strait/main.py
+++ malacca_strait/main.py
@@ -1,6 +1,11 @@
 def main():
     print("Hello from malacca-strait!")
 
+    # multiselect for layers (bathymetry, currents, chl)
+
+    # multiselect for date 
+
+    
 
 if __name__ == "__main__":
     main()
